6615be5ad42c6cf80a850d845562afb9817f40bb
Renan Melo <melorenan149@gmail.com>
2021-04-07 15:40:57 -0300

    com defeito

---
+++ Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstAPI.Models
+{
+    public class TodoItem
+    {
+        public long ID { get; set; }
+        public string nome { get; set; }
+        public bool IsComplete { get; set; }
+
+    }
+}
